26cf8e67f71c8645cac7f9d8c45377c98adf1124
tedthavisin <teddyt@unc.edu>
2024-11-30 19:23:22 -0500

    Added 'Buttons and State' & 'List and Text field files' from tutorial.

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Ted Thavisin on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pick-A-Pal/Pick-A-Pal/Pick_A_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_A_PalApp.swift
+//  Pick-A-Pal
+//
+//  Created by Ted Thavisin on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Pick_A_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
